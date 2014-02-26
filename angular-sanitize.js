7976f909ee674a37477ed2beb929b4a61f52e7a2
jenkins <ci-bot@angularjs.org>
2014-02-26 04:16:00 -0800

    v1.2.14-build.2326+sha.332e935

--- angular-sanitize.js
+++ angular-sanitize.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.2.14-build.2325+sha.3d6dff4
+ * @license AngularJS v1.2.14-build.2326+sha.332e935
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -399,7 +399,7 @@ function decodeEntities(value) {
  * resulting string can be safely inserted into attribute or
  * element text.
  * @param value
- * @returns escaped text
+ * @returns {string} escaped text
  */
 function encodeEntities(value) {
   return value.
