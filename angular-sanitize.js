2b1727be28aa42cc0c8ed0d74ea5a44f6ae5afaf
jenkins <ci-bot@angularjs.org>
2014-06-06 13:43:10 -0700

    v1.2.18-build.233+sha.e906aaf

--- angular-sanitize.js
+++ angular-sanitize.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.3.0-build.2804+sha.560f008
+ * @license AngularJS v1.2.18-build.233+sha.e906aaf
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
