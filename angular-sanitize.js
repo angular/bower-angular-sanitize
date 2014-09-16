27f36e1636e32915bd80df7715af38a25d30dbde
jenkins <ci-bot@angularjs.org>
2014-09-16 15:22:53 -0700

    v1.2.25-build.457+sha.f6f0791

--- angular-sanitize.js
+++ angular-sanitize.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.3.0-build.3261+sha.14a92a5
+ * @license AngularJS v1.2.25-build.457+sha.f6f0791
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
