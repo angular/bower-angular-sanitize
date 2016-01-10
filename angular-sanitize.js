e5e365e1162cd25c40d9bfa558c688005ac6a7a9
jenkins <ci-bot@angularjs.org>
2016-01-10 12:43:02 -0800

    v1.5.0-build.4493+sha.8ccc054

--- angular-sanitize.js
+++ angular-sanitize.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.5.0-build.4491+sha.8b63603
+ * @license AngularJS v1.5.0-build.4493+sha.8ccc054
  * (c) 2010-2016 Google, Inc. http://angularjs.org
  * License: MIT
  */
