27b6da5578b99482330c6c38c5039a517e777c47
jenkins <ci-bot@angularjs.org>
2017-08-18 07:37:28 -0700

    v1.6.6

--- angular-sanitize.js
+++ angular-sanitize.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.6.6-build.5452+sha.6d2ff0a
+ * @license AngularJS v1.6.6
  * (c) 2010-2017 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -218,7 +218,7 @@ function $SanitizeProvider() {
   extend = angular.extend;
   forEach = angular.forEach;
   isDefined = angular.isDefined;
-  lowercase = angular.$$lowercase;
+  lowercase = angular.lowercase;
   noop = angular.noop;
 
   htmlParser = htmlParserImpl;
@@ -604,7 +604,7 @@ function sanitizeText(chars) {
 // define ngSanitize module and register $sanitize service
 angular.module('ngSanitize', [])
   .provider('$sanitize', $SanitizeProvider)
-  .info({ angularVersion: '1.6.6-build.5452+sha.6d2ff0a' });
+  .info({ angularVersion: '1.6.6' });
 
 /**
  * @ngdoc filter
