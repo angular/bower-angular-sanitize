d0ca6613b9866ac1589c9448024c54e55b4ab727
jenkins <ci-bot@angularjs.org>
2018-04-19 02:36:21 -0700

    v1.6.11-build.5554+sha.841feb0

--- angular-sanitize.js
+++ angular-sanitize.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.7.0-rc.0
+ * @license AngularJS v1.6.11-build.5554+sha.841feb0
  * (c) 2010-2018 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -711,7 +711,7 @@ function sanitizeText(chars) {
 // define ngSanitize module and register $sanitize service
 angular.module('ngSanitize', [])
   .provider('$sanitize', $SanitizeProvider)
-  .info({ angularVersion: '1.7.0-rc.0' });
+  .info({ angularVersion: '1.6.11-build.5554+sha.841feb0' });
 
 /**
  * @ngdoc filter
