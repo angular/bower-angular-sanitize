1369c485264d7102924aba0ac732acbccd3533d6
jenkins <ci-bot@angularjs.org>
2018-06-08 07:31:38 -0700

    v1.7.1

--- angular-sanitize.js
+++ angular-sanitize.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.7.0
+ * @license AngularJS v1.7.1
  * (c) 2010-2018 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -711,7 +711,7 @@ function sanitizeText(chars) {
 // define ngSanitize module and register $sanitize service
 angular.module('ngSanitize', [])
   .provider('$sanitize', $SanitizeProvider)
-  .info({ angularVersion: '1.7.0' });
+  .info({ angularVersion: '1.7.1' });
 
 /**
  * @ngdoc filter
